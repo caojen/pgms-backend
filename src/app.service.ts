4f108b59ccb0dbd74b41518bf07df6cf42d9edf7
caojen <caojen@mail2.sysu.edu.cn>
2020-10-06 18:53:24 +0800

    create the inpath of browers

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getFrontend(): string {
+    // TODO: render to frontend
+    return 'hello world';
   }
 }
