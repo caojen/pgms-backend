1761e2900cdf125aa3e658787ef99aa0ae7bc5c0
caojen <caojen@mail2.sysu.edu.cn>
2020-10-09 12:30:28 +0800

    remove unuse imports

--- src/teacher/teacher.module.ts
+++ src/teacher/teacher.module.ts
@@ -1,4 +1,4 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { TeacherService } from './teacher.service';
 import { TeacherController } from './teacher.controller';
 import { UserService } from 'src/user/user.service';
