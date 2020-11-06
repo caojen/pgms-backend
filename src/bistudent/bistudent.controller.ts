5c9b8737c0aad680ccb446b73c39d5759be36f3f
caojen <caojen@mail2.sysu.edu.cn>
2020-11-06 23:37:01 +0800

    bistudent most

--- src/bistudent/bistudent.controller.ts
+++ src/bistudent/bistudent.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Put, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Put, Req, UseGuards } from '@nestjs/common';
 import { LoginRequired } from 'src/user/user.guard';
 import { BistudentPermission } from './bistudent.guard';
 import { BistudentService } from './bistudent.service';
@@ -10,12 +10,66 @@ export class BistudentController {
     private readonly bistudentService: BistudentService
   ) {}
 
+  /**
+   * @api {put} /bistudent GetBiChoiceInfo
+   * @apiName GetBiChoiceInfo
+   * @apiGroup Bistudent
+   * @apiPermission Logined Bistudent
+   * @apiSuccessExample {json} Success-Response
+    {
+      "begin_time": "{\"value\":\"2020-11-6 23:16:00\"}",
+      "current_stage": "{\"value\":-1}",
+      "end_time": "{\"value\":\"2020-11-8 23:16:00\"}",
+      "stage_count": "{\"value\":3}"
+    }
+   */
+  @Get('')
+  @UseGuards(LoginRequired, BistudentPermission)
+  async getBiChoiceInfo() {
+    return this.bistudentService.getBiChoiceInfo();
+  }
+
+  /**
+   * @api {get} /bistudent/info GetBistudentInfo
+   * @apiName GetBistudentInfo
+   * @apiGroup Bistudent
+   * @apiPermission Logined Bistudent
+   * @apiSuccessExample {json} Success-Response
+    {
+      "id": 1,
+      "name": "biname",
+      "recommended": 0,
+      "score": 396,
+      "graduation_university": "university",
+      "graduation_major": "abc",
+      "household_register": "ddd",
+      "ethnic": "ethnic",
+      "phone": "newphone",
+      "gender": "x",
+      "email": "email@qq.com",
+      "source_des": "985",
+      "degree_des": "degree1",
+      "enrol_des": "enrol1",
+      "image": 1,
+      "selected_teachers": "[]"
+    }
+   */
   @Get('info')
   @UseGuards(LoginRequired, BistudentPermission)
   async getInfo(@Req() request) {
     return request.user.bistudent;
   }
 
+  /**
+   * @api {put} /bistudent/info UpdateBistudentInfo
+   * @apiName UpdateBistudentInfo
+   * @apiGroup Bistudent
+   * @apiPermission Logined Bistudent
+   * @apiSuccessExample {json} Success-Response
+    {
+      msg: '修改信息成功'
+    }
+   */
   @Put('info')
   @UseGuards(LoginRequired, BistudentPermission)
   async updateInfo(@Req() request, @Body() body: {
@@ -24,4 +78,40 @@ export class BistudentController {
   }) {
     return await this.bistudentService.updateInfo(request.user.bistudent.id, body);
   }
+
+  /**
+   * @api {get} /bistudent/teachers/all GetAllTeachers
+   * @apiName GetAllTeachers
+   * @apiGroup Bistudent
+   * @apiPermission Logined Bistudent
+   * @apiSuccessExample {json} Success-Response
+    {
+      msg: '修改信息成功'
+    }
+   */
+  @Get('teachers/all')
+  @UseGuards(LoginRequired, BistudentPermission)
+  async getAllTeachers(@Req() request) {
+    const bid = request.user.bistudent.id;
+    return await this.bistudentService.getAllTeachers(bid);
+  }
+
+  @Put('teacher/:tid')
+  @UseGuards(LoginRequired, BistudentPermission)
+  async selectOneTeacher(@Req() request, @Param() param: {tid: string}) {
+    const bid = request.user.bistudent.id;
+    const tid = parseInt(param.tid);
+
+    return await this.bistudentService.selectOneTeacher(bid, tid);
+  }
+
+  @Delete('teacher/:tid')
+  @UseGuards(LoginRequired, BistudentPermission)
+  async deleteOneTeacher(@Req() request, @Param() param: {tid: string}) {
+    const bid = request.user.bistudent.id;
+    const tid = parseInt(param.tid);
+
+    return await this.bistudentService.deleteOneTeacher(bid, tid);
+  }
+
 }
--- src/bistudent/bistudent.service.ts
+++ src/bistudent/bistudent.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { QueryDbService } from 'src/query-db/query-db.service';
 
 @Injectable()
@@ -8,7 +8,32 @@ export class BistudentService {
     private readonly dbQuery: QueryDbService
   ) {}
 
-  async updateInfo(id, {phone, email}) {
+  async getBiChoiceInfo() {
+    /**
+      stage_count: number; 表示双选时总共的阶段数(这也是学生可以选择的老师总数)
+      current_stage: number; 表示当前的阶段数. 当值为-1时代表未开始, 当值为0是代表学生选择阶段(1,2,3...等代表老师选择阶段) 当值大于stage_count时代表已结束
+      begin_time: string; 一个可以格式化为date的字符串, 代表当前阶段开始时间
+      end_time: string; 一个可以格式化为date的字符串, 代表当前阶段的结束时间 // 当当前时间位于两个时间之间时, 代表某个身份可以选择
+     */
+    const sql = `
+      SELECT \`key\`, \`value\`
+      FROM settings;
+    `;
+
+    const settings = await this.dbQuery.queryDb(sql, []);
+    const returns = ["stage_count", "current_stage", "begin_time", "end_time"];
+    const res = {};
+    for(const index in settings) {
+      const setting = settings[index];
+      if(returns.indexOf(setting.key) !== -1) {
+        res[setting.key] = setting.value;
+      }
+    }
+
+    return res;
+  }
+
+  async updateInfo(id: number, {phone, email}) {
     const sql = `
       UPDATE bistudent
       SET phone = ?, email = ?
@@ -18,6 +43,146 @@ export class BistudentService {
     await this.dbQuery.queryDb(sql, [phone, email, id]);
     return {
       msg: '修改信息成功'
+    };
+  }
+
+  async getAllTeachers(id: number) {
+    // get degree:
+    const degreeSql = `
+      SELECT degree
+      FROM bistudent
+      WHERE id=?;
+    `;
+    const degree_id = (await this.dbQuery.queryDb(degreeSql, [id]))[0].degree;
+
+    // get enrol:
+    const enrolSql = `
+      SELECT enrol
+      FROM degree
+      WHERE id=?;
+    `;
+    const enrol_id = (await this.dbQuery.queryDb(enrolSql, [id]))[0].enrol;
+
+    const teachersSql = `
+      SELECT id, name, email, personal_page, research_area, bichoice_config
+      FROM teacher;
+    `;
+
+    const teachers = await this.dbQuery.queryDb(teachersSql, []);
+    
+    const res = [];
+
+    for(const index in teachers) {
+      const teacher = teachers[index];
+      const config = JSON.parse(teacher.bichoice_config);
+      const degrees = config["degrees"];
+      for(const dindex in degrees) {
+        const degree = degrees[dindex];
+        if(degree.id === degree_id && degree.num > 0) {
+
+          let canChoose = false;
+          for(const eindex in config["enrols"]) {
+            const enrol = config["enrols"][eindex];
+            if(enrol.id == enrol_id) {
+              if(enrol.num > 0) {
+                canChoose = true;
+              }
+            }
+            break;
+          }
+
+          if(canChoose) {
+            res.push({
+              id: teacher.id,
+              name: teacher.name,
+              email: teacher.email,
+              personal_page: teacher.personal_page,
+              research_area: teacher.research_area
+            });
+          }
+          break;
+        }
+      }
+    }
+
+    return res;
+  }
+
+  async selectOneTeacher(bid: number, tid: number) {
+    const teachers = await this.getAllTeachers(bid);
+    let canChoose = false;
+    for(const index in teachers) {
+      const teacher = teachers[index];
+      if(teacher.id === tid) {
+        canChoose = true;
+        break;
+      }
+    }
+
+    if(canChoose === false) {
+      throw new HttpException({
+        msg: '该老师不在可选范围之内'
+      }, 406);
+    }
+
+    const selectSql = `
+      SELECT selected_teachers
+      FROM bistudent
+      WHERE id=?;
+    `;
+
+    const selected_teachers = JSON.parse(
+      (await this.dbQuery.queryDb(selectSql, [bid]))[0].selected_teachers
+    );
+    if(selected_teachers.indexOf(tid) !== -1) {
+      throw new HttpException({
+        msg: '该老师已被选择, 无需重复选择'
+      }, 406);
+    }
+
+    selected_teachers.push(tid);
+    const length = selected_teachers.length;
+    const updateSql = `
+      UPDATE bistudent
+      SET selected_teachers=?
+      WHERE id=?;
+    `;
+    await this.dbQuery.queryDb(updateSql, [JSON.stringify(selected_teachers), bid]);
+    return {
+      msg: '选择成功',
+      tid,
+      length,
+    };
+  }
+
+  async deleteOneTeacher(bid: number, tid: number) {
+    const selectSql = `
+      SELECT selected_teachers
+      FROM bistudent
+      WHERE id=?;
+    `;
+
+    const selected_teachers: number[] = JSON.parse(
+      (await this.dbQuery.queryDb(selectSql, [bid]))[0].selected_teachers
+    );
+
+    const index = selected_teachers.indexOf(tid);
+    if(index === -1) {
+      throw new HttpException({
+        msg: '未选择该老师, 无法取消选择'
+      }, 406);
+    }
+
+    selected_teachers.splice(index, 1);
+    const updateSql = `
+      UPDATE bistudent
+      SET selected_teachers=?
+      WHERE id=?;
+    `;
+    await this.dbQuery.queryDb(updateSql, [JSON.stringify(selected_teachers), bid]);
+    return {
+      msg: '取消选择成功',
+      selected_teachers
     };
   }
 }
