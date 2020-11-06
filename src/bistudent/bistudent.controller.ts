a720cf821c6bf0d8912e222ee5e82b1f71d67fbf
caojen <caojen@mail2.sysu.edu.cn>
2020-11-06 22:09:13 +0800

    reinit because of bichoice

--- src/bistudent/bistudent.controller.ts
+++ src/bistudent/bistudent.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Put, Req, UseGuards } from '@nestjs/common';
 import { LoginRequired } from 'src/user/user.guard';
 import { BistudentPermission } from './bistudent.guard';
+import { BistudentService } from './bistudent.service';
 
 @Controller('bistudent')
 export class BistudentController {
+
+  constructor(
+    private readonly bistudentService: BistudentService
+  ) {}
+
   @Get('info')
   @UseGuards(LoginRequired, BistudentPermission)
-  async getInfo() {
-    return;
+  async getInfo(@Req() request) {
+    return request.user.bistudent;
+  }
+
+  @Put('info')
+  @UseGuards(LoginRequired, BistudentPermission)
+  async updateInfo(@Req() request, @Body() body: {
+    phone: string,
+    email: string,
+  }) {
+    return await this.bistudentService.updateInfo(request.user.bistudent.id, body);
   }
 }
--- src/bistudent/bistudent.service.ts
+++ src/bistudent/bistudent.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { QueryDbService } from 'src/query-db/query-db.service';
 
 @Injectable()
-export class BistudentService {}
+export class BistudentService {
+
+  constructor(
+    private readonly dbQuery: QueryDbService
+  ) {}
+
+  async updateInfo(id, {phone, email}) {
+    const sql = `
+      UPDATE bistudent
+      SET phone = ?, email = ?
+      WHERE id = ?;
+    `;
+
+    await this.dbQuery.queryDb(sql, [phone, email, id]);
+    return {
+      msg: '修改信息成功'
+    };
+  }
+}
