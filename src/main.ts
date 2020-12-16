a422907a2fc24ce11148960345c2b8852b273052
caojen <caojen@mail2.sysu.edu.cn>
2020-12-16 17:05:23 +0800

    change allow origin

--- src/main.ts
+++ src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
   // 允许跨域：
   app.use('/', function(req, res: express.Response, next) {
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Origin', 'localhost:8080');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
     res.setHeader('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
     next();
