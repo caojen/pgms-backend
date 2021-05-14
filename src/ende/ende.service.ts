e6beadf0496a321a952ef4226919d7e3258aadb8
曹鉴恩 <caojen@mail2.sysu.edu.cn>
2021-05-14 13:00:08 +0800

    ende test: use pbkdf2_sha256

--- src/ende/ende.service.ts
+++ src/ende/ende.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+
+import * as pbkdf2 from 'pbkdf2';
 
 @Injectable()
 export class EndeService {
@@ -16,26 +18,38 @@ export class EndeService {
 
   /**
    * encode plaintext password to encrypted password
-   * @param password 
+   * @param password plain text
    */
   static encodeToDatabase(password: string): string {
-    return password;
+    const salt = this.createNewToken(12);
+    const iteration = 10000;
+    const hash = pbkdf2.pbkdf2Sync(password, salt, iteration, 32).toString();
+
+    return `pbkdf2_sha256$${iteration}$${salt}$${hash}`;
   }
 
   /**
    * try to verity the password is valid(same as the database password)
-   * @param password 
-   * @param hash 
+   * @param password plain text
+   * @param hash hashed password that stored in database
    */
   static verify(password: string, hash: string): boolean {
-    return password === hash;
+    // return password === hash;
+    const parts = hash.split('$');
+    if(parts.length !== 4) {
+      return hash === password;
+    }
+    const iteration = parseInt(parts[1]);
+    const salt = parts[2];
+    const password_hash = pbkdf2.pbkdf2Sync(password, salt, iteration, 32).toString();
+    return password_hash === parts[3];
   }
 
   private static readonly charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  private static readonly tokenlen = 64;
-  static createNewToken(): string {
+  // private static readonly tokenlen = 64;
+  static createNewToken(len: number = 64): string {
     let str = '';
-    for(let i = 0;i<this.tokenlen;i++) {
+    for(let i = 0;i<len;i++) {
       const index = Math.random() * this.charset.length;
       str += this.charset.charAt(index);
     }
