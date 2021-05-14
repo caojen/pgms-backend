bea9662e18060b77cbe0378832d41f2aa3429af6
曹鉴恩 <caojen@mail2.sysu.edu.cn>
2021-05-14 13:11:31 +0800

    add hash_password script

---
+++ scripts/hash_password.js
@@ -0,0 +1,20 @@
+const pbkdf2 = require('pbkdf2');
+
+const plain_text = 'password';
+
+const salt_char_set = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+const salt_len = 12;
+
+const iteration = 10000;
+
+// generate salt:
+let salt = '';
+for (let i = 0; i < salt_len; i++) {
+  const index = Math.random() * salt_len;
+  salt += salt_char_set.charAt(index);
+}
+
+const hash = pbkdf2.pbkdf2Sync(plain_text, salt, iteration, 32).toString('base64');
+const output = `pbkdf2_sha256$${iteration}$${salt}$${hash}`;
+
+console.log(output);
